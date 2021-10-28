16c51f7af887dfd1aa1a640b22b84d7a3d34c8bf
michelle-mok <73230024+michelle-mok@users.noreply.github.com>
2021-10-28 23:30:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,28 @@ const whenIncomingRequest = (request, response) => {
 
   var filePath = '.' + request.url;
 
+  var mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.svg': 'image/svg+xml',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.wasm': 'application/wasm'
+    };
+
+  var contentType = mimeTypes[extname] || 'application/octet-stream';
+  
   readFile(filePath, (error, content) => {
-    response.writeHead(200, { 'Content-Type': 'text/css' });
+    response.writeHead(200, { 'Content-Type': contentType });
     response.end(content, 'utf-8');
   });
 };
